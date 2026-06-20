8783f7b1be39aab2d9c44f78b49acf8e5a6e8955
bhojeshwarsahu <bhojeshwarsahu123@gmail.com>
2026-06-20 16:06:20 +0530

    feat: update IAM role policies to use dynamic repository references and clean up commented code

--- locals.tf
+++ locals.tf
@@ -2,6 +2,7 @@ locals {
   name_prefix       = "${var.project_name}-${var.environment}"
   account_id        = data.aws_caller_identity.current.account_id
   github_repository = "${var.github_owner}/aws-platform-bootstrap"
+  github_infra_repository = "${var.github_owner}/aws-platform-infra"
 
   common_tags = {
     Project     = var.project_name
@@ -12,7 +13,7 @@ locals {
 
   terraform_state_bucket = "${local.name_prefix}-${local.account_id}-tf-state"
 
-  terraform_lock_table = "${local.name_prefix}-tf-lock"
+  # terraform_lock_table = "${local.name_prefix}-tf-lock"
 
   kms_alias = "alias/${local.name_prefix}-tf-state"
 }
